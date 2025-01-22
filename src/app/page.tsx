acb832bed085ece33c9bc655e4f4ea03ff4afc5d
Paige <paige.struthers@gmail.com>
2025-01-22 16:22:32 -0600

    added tiptap integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
-  return ( 
+  return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>Click me</Button>
+      Click
+      <Link href="/documents/123">
+        &nbsp;<span className="text-blue-500 underline">here</span>&nbsp;
+      </Link>
+      to go to document ID page
     </div>
-   );
+  );
 };
- 
+
 export default Home;
