24dea1d05acc6daf66208a771d49f5216105275e
Paige <paige.struthers@gmail.com>
2025-02-04 16:15:32 -0600

    delete documents

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,47 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Id } from "../../../convex/_generated/dataModel";
+import { Button } from "@/components/ui/button";
+import { ExternalLinkIcon, MoreVertical, TrashIcon } from "lucide-react";
+import { RemoveDialog } from "@/components/remove-dialog";
+
+interface DocumentMenuProps {
+  documentId: Id<"documents">;
+  title: string;
+  onNewTab: (id: Id<"documents">) => void;
+}
+
+export const DocumentMenu = ({
+  documentId,
+  title,
+  onNewTab,
+}: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <MoreVertical className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+          <ExternalLinkIcon className="size-4 mr-2" />
+          Open in a new tab
+        </DropdownMenuItem>
+        <RemoveDialog documentId={documentId}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <TrashIcon className="size-4 mr-2" />
+            Remove
+          </DropdownMenuItem>
+        </RemoveDialog>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
