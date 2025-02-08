84283e319db1a8157f9376bf8136a219030eff99
Paige <paige.struthers@gmail.com>
2025-02-07 20:41:30 -0600

    notifications feature

--- convex/documents.ts
+++ convex/documents.ts
@@ -69,6 +69,25 @@ export const getById = query({
 	},
 });
 
+export const getByIds = query({
+	args: { ids: v.array(v.id("documents")) },
+	handler: async (ctx, { ids }) => {
+		const documents = [];
+
+		for (const id of ids) {
+			const document = await ctx.db.get(id);
+
+			if (document) {
+				documents.push({ id: document._id, name: document.title });
+			} else {
+				documents.push({ id, name: "[Removed]" })
+			}
+		}
+
+		return documents;
+	},
+});
+
 export const updateById = mutation({
 	args: {
 		id: v.id("documents"),
--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -20,6 +20,7 @@ import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Avatars } from "./avatars";
+import { Inbox } from "./inbox";
 
 export const Navbar = () => {
 	const { editor } = useEditorStore();
@@ -244,6 +245,7 @@ export const Navbar = () => {
       </div>
       <div className="flex items-center gap-3 pl-4">
         <Avatars />
+        <Inbox />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
